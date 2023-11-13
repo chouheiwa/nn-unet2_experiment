43dfacbf53054e97b28b6cc1c36e625f20e041e9
chouheiwa <849131492@qq.com>
2023-11-13 17:09:30 +0800

    Update pip repository from github to gitee

---
+++ setup.py
@@ -0,0 +1,6 @@
+import subprocess
+
+if __name__ == '__main__':
+    subprocess.run(['pip install git+https://gitee.com/wd_kind/MobileSAM.git'])
+    subprocess.run(['pip install timm'])
+    subprocess.run(['pip install git+https://github.com/chouheiwa/nnSAM.git'])
